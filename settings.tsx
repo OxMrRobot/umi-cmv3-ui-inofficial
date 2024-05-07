924c71c96ced0ba1a0f8e33465fd7f4772bd99c8
0xMrRobot <35005026+OxMrRobot@users.noreply.github.com>
2024-05-07 16:15:16 +0200

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,14 +1,14 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "Wls", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "OGs", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "Public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
+  //{ label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://raw.githubusercontent.com/OxMrRobot/PixizTest/main/UMnM1Hp-_400x400.jpg";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "PlayPixiz mint UI";
