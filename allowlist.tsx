0cc2b78d8e570dbed3d7b4456350d92a89ffb3a1
0xMrRobot <35005026+OxMrRobot@users.noreply.github.com>
2024-05-07 18:27:36 +0200

    Update allowlist.tsx

--- allowlist.tsx
+++ allowlist.tsx
@@ -1,17 +1,17 @@
 //add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
 //You can also add them here and copy the route from the console log 
 export const allowLists = new Map<string, Array<string>> ([
-    ["WL",
+    ["Wls",
     [
       "Cqqm1SmwfAyVvoVazMUt2dU77JhPqX6QtRdNxECsiByn",
       "DCspvFnZj5PP5RteEtcuotAVV3y7XxmYUSoSKetQkXEQ",
       "6i52UsnnWSoqcd6EAuhEZyZ55bbBcvT5ZDFyuXwAJ8vz"
     ]],
-    ["OG",
+    ["OGs",
     [
       "3Q8jpxqaPLnVhwMAmdzFWD1vVrz9Eg8KdV16vQtScSog",
       "64vJEHbsjX9MoR23PYdDBU9dthYERh2Q9fuEEMwRgsWQ",
-      "HMTLkqBkz3a9TPkgprfAmhdzwFnNQVeWMWN4P2agtKhU"
+      "HMTLkqBkz3a9TPkgprfAmhdzwFnNQVeWMWN4P2agtKhU",
     ]],
     /*["default",
     [
